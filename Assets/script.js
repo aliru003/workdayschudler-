ae3a59461152b3d2379873b02fbde52cffb00739
aliru003 <ALI@ALIRUZICKA.COM>
2021-09-10 17:15:25 -0500

    updates

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+// getting current time and day using moment.js and displaying in the <p> tag with id of current-day
+$("#current-day").text(moment().format("MMMM Do YYYY, h:mm a"));
+
+
+
+
+})
+
+
+
+
+
